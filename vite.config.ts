46a6abe0c234b8904994b0d42492e94b683f0e3d
Juan Francisco Pintar <juanfranciscopintar@protonmail.com>
2026-05-25 02:41:51 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/cabrankeditor/',
+  base: '/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
